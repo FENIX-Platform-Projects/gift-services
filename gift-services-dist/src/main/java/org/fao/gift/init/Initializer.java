26a140f5f619a29dc6c111fa3a90b2bd225dfe35
fabriziofenix <fabrizio.castelli@fao.org>
2018-01-26 17:55:49 +0100

    add swagger-ui

--- gift-services-dist/src/main/java/org/fao/gift/init/Initializer.java
+++ gift-services-dist/src/main/java/org/fao/gift/init/Initializer.java
@@ -3,6 +3,8 @@ package org.fao.gift.init;
 import org.fao.ess.uploader.core.init.UploaderConfig;
 import org.fao.gift.common.dto.MainConfig;
 import org.fao.gift.common.utils.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.servlet.ServletContextEvent;
@@ -15,11 +17,14 @@ public class Initializer implements ServletContextListener {
     @Inject MainConfig config;
     @Inject UploaderConfig uploaderConfig;
     @Inject DataSource dataSource;
+    private static final Logger log = LoggerFactory.getLogger(Initializer.class);
+
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
             config.init(this.getClass().getResourceAsStream("/org/fao/gift/config/main.properties"));
+            log.info("config init!!!");
             uploaderConfig.init(this.getClass().getResourceAsStream("/org/fao/gift/config/upload.properties"));
             dataSource.init(config.get("gift.db.url"),config.get("gift.db.usr"),config.get("gift.db.psw"));
         } catch (Exception e) {
